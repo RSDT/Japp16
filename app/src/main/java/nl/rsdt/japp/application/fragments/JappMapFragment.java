926a785fd354b455e6dcbf15e77ef24ef4e13d44
Dingenis <stesin@live.com>
2017-10-11 20:12:57 +0200

    changes in clustering

--- app/src/main/java/nl/rsdt/japp/application/fragments/JappMapFragment.java
+++ app/src/main/java/nl/rsdt/japp/application/fragments/JappMapFragment.java
@@ -371,7 +371,7 @@ public class JappMapFragment extends Fragment implements IJotiMap.OnMapReadyCall
         movementManager.onMapReady(jotiMap);
 
         setupDeelgebieden();
-        
+
         pinningManager.onMapReady(jotiMap);
         if(callback != null)
         {
